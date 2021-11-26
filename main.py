90c45a8c3c393e19494fed4fce7fca7ed08af3df
Deevanshi Sharma <87795451+DeevanshiSharma@users.noreply.github.com>
2021-11-27 03:18:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import smtplib
 
 # put in the url of product whose price you want to track
-Url = "copy_the_url"
+Url = "copy_the_url_of_the_product"
 # change header according to your device
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'}
 
